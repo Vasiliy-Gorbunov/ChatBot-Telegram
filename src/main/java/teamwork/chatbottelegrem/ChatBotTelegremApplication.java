7bdf65b5d59ad69aeb58fbf506b1488d1ca84f7a
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2023-04-07 23:46:22 +0300

    Создание проекта

---
+++ src/main/java/teamwork/chatbottelegrem/ChatBotTelegremApplication.java
@@ -0,0 +1,13 @@
+package teamwork.chatbottelegrem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotTelegremApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotTelegremApplication.class, args);
+    }
+
+}
