1335f20d562fd0d21917e55735fb90437b42270a
Vasiliy-Gorbunov <79896130110@yandex.ru>
2023-04-18 17:28:28 +0300

    first method

--- src/main/java/teamwork/chatbottelegrem/service/TelegramBotService.java
+++ src/main/java/teamwork/chatbottelegrem/service/TelegramBotService.java
@@ -1,6 +1,6 @@
 package teamwork.chatbottelegrem.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import teamwork.chatbottelegrem.repository.TelegramBotRepository;
 
