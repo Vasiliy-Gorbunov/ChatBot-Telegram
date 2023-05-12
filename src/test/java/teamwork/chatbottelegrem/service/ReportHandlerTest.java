5c5fd6edee76f13ed36c1488cf967d9e8291a4fd
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2023-05-12 15:13:14 +0500

    Покрыл тестами 2 класса сервисов

--- src/test/java/teamwork/chatbottelegrem/service/ReportHandlerTest.java
+++ src/test/java/teamwork/chatbottelegrem/service/ReportHandlerTest.java
@@ -4,6 +4,7 @@ import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,33 +21,44 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class ReportHandlerTest {
-//    String json = Files.readString(Path.of(ReportHandlerTest.class.getResource("update.json").toURI()));
-//
-//    @Mock
-//    TelegramBot telegramBot;
-//    @Mock
-//    Logger logger;
-//    @InjectMocks
-//    ReportHandler reportHandler;
-//@BeforeEach
-//public void iniMocks() {
-//    MockitoAnnotations.initMocks(this);
-//}
-//
-//    ReportHandlerTest() throws IOException, URISyntaxException {
-//
-//    }
-//
-//
-//    @Test
-//    void checkReport() {
-//        Update update = BotUtils.fromJson(json.replace("%text%", ""), Update.class);
-//
-//        reportHandler.checkReport(update);
-//        SendMessage sendMessage = new SendMessage(update.message().chat().id(), "Пожалуйста, направьте текстовый отчет о питомце");
-//        verify(telegramBot).execute(sendMessage);
-//
-//    }
+
+
+    @Mock
+    TelegramBot telegramBot;
+    @Mock
+    Logger logger;
+    @InjectMocks
+    ReportHandler reportHandler;
+@BeforeEach
+public void iniMocks() {
+    MockitoAnnotations.initMocks(this);
+}
+
+    ReportHandlerTest() throws IOException, URISyntaxException {
+
+    }
+
+
+    @Test
+    void checkReport() throws URISyntaxException, IOException {
+        String json = Files.readString(Path.of(ReportHandlerTest.class.getResource("update.json").toURI()));
+        Update update = BotUtils.fromJson(json.replace("%text%", " "), Update.class);
+        SendMessage sendMessage = new SendMessage(update.message().chat().id(), "Пожалуйста, направьте текстовый отчет о питомце");
+        SendResponse sendResponse = BotUtils.fromJson("""
+                {
+                "result": 200,
+                "ok": true
+                }
+                """, SendResponse.class);
+
+        when(telegramBot.execute(sendMessage)).thenReturn(sendResponse);
+        reportHandler.checkReport(update);
+
+        verify(telegramBot).execute(sendMessage);
+
+    }
 }
